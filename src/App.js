df89e15db320959d2ca943cd7b617b8fcfa06d94
Qu1te <148548462+Qu1te@users.noreply.github.com>
2024-04-12 01:50:12 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {createBrowserRouter,RouterProvider } from "react-router-dom"
+import Main from "./pages/Main";
+function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main/>,
+    }
+  ]);
+  return (
+    <>
+      <RouterProvider  router = {router}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Footer = () => {
+    return(
+        <footer className="FooterConteiner">
+            <div className="FooterContent">
+                <p>Copyright © 2024 Play Hub Gaming Company. All rights reserved.    <a href="/">Design: TemplateMo</a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
